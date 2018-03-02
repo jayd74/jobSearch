bd17faa86fd587a12fb5f1cab553d9a95dde0c73
Jonathan Fejtek <jonathan.fejtek@mail.utoronto.ca>
2018-03-02 10:40:23 -0500

    added routing with props

--- dev/scripts/jobsview.js
+++ dev/scripts/jobsview.js
@@ -4,9 +4,19 @@ import ReactDOM from 'react-dom';
 class JobsView extends React.Component{
     constructor(props){
         super(props);
+        console.log(this.props.jobsAppliedFor);
     }
 
     render(){
-        
+        return (
+            <div>
+                <h2>Jobs Applied For:</h2>
+                {Object.values(this.props.jobsAppliedFor).map((item)=>{
+                    return <h3>{item.company}</h3>
+                })}
+            </div>
+        );
     }
 }
+
+export default JobsView;
